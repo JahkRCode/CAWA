8b0d57b4050eee72af936ebb0775b65d88845b6b
JahkRCode <JahkRCode@safires.org>
2020-03-27 20:00:08 -0700

    Initial Commit
    Updated README
    Created python file

--- CAWA.py
+++ CAWA.py
@@ -5,10 +5,3 @@
 ## Date Created: March 27, 2020
 #######################################################
 
-The purpose of this module is to analyze 
-the Novel Covid-19 dataset provided by 
-Johns Hopskins University for Systems Science and Engineering (JHU CSSE)
-using a deep learning model.
-
-Source:
-https://github.com/CSSEGISandData/COVID-19
